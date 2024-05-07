299ec714a2c606a6b390cdef68b3f2e6ff337092
Anya Aven <leafeon96@gmail.com>
2024-05-07 15:28:54 -0400

    Added User model

--- models.py
+++ models.py
@@ -5,3 +5,37 @@ db = SQLAlchemy()
 dbx = db.session.execute
 
 bcrypt = Bcrypt()
+
+class User(db.Model):
+    """ Site user """
+
+    __tablename__ = "users"
+
+    username = db.mapped_column(
+        db.String(20),
+        db.Identity(),
+        primary_key=True,
+    )
+
+    hashed_password = db.mapped_column(
+        db.String(100),
+        nullable=False,
+    )
+
+    email = db.mapped_column(
+        db.String(50),
+        nullable=False,
+        unique=True,
+    )
+
+    first_name = db.mapped_column(
+        db.String(30),
+        nullable=False,
+    )
+
+    last_name = db.mapped_column(
+        db.String(30),
+        nullable=False,
+    )
+
+
