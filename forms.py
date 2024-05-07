aa87513c6bc0914cdfc95bed05ae1f7419f30b89
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-05-07 13:55:09 -0700

    updated methods on register form

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, EmailField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms.validators import InputRequired, Email, Length, ValidationError
+
 
 class RegisterForm(FlaskForm):
     """Form for registering a user."""
@@ -46,6 +47,15 @@ class RegisterForm(FlaskForm):
         ]
     )
 
+    def validate_username(self, username):
+        # TODO: double check the username.data part
+        """Checks if the username exists in the database already.
+        If so, raises a validation error."""
+        user = self.query.filter_by(username=username.data).first()
+        if user:
+            return False
+        return True
+
 
 # FIXME: LoginForm was copied from hash lecture
 
@@ -54,13 +64,14 @@ class LoginForm(FlaskForm):
 
     username = StringField(
         "Username",
-        validators=[InputRequired()] # FIXME: add the correct validators
+        validators=[InputRequired()]  # FIXME: add the correct validators
     )
 
     password = PasswordField(
         "Password",
-        validators=[InputRequired()] # FIXME: add the correct validators
+        validators=[InputRequired()]  # FIXME: add the correct validators
     )
+
 
 class CSRFProtectForm(FlaskForm):
     """Form just for CSRF Protection"""
